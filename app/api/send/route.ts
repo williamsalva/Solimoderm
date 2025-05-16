a08519bd291d8a57a283cedf4c24dc61de17de72
williamsalva <williams.alva@hotmail.com>
2025-05-16 16:59:03 -0600

    doble correo

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
 
     const { data, error } = await resend.emails.send({
       from: "hola@solimoderm.com",
-      to: "solimodermcontacto@gmail.com",
+      to: ["solimodermcontacto@gmail.com", "ricardosolimoderm@hotmail.com"],
       subject: "¡Tienes un nuevo mensaje en tu pagina Solimoderm.com!",
       react: EmailTemplate({ name, email, phone, city, message }),
     })
